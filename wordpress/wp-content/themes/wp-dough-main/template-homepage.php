7d097a24b466321d0a44ddc1559629a4313eb131
AbdullatifAhmad <78532689+AbdullatifAhmad@users.noreply.github.com>
2022-05-31 14:43:21 +0200

    home page
    hier hebben we de home page gamaakt

---
+++ wordpress/wp-content/themes/wp-dough-main/template-homepage.php
@@ -0,0 +1,29 @@
+<?php
+/*
+ * Template Name: home page
+ */
+
+$title = get_the_title();
+$content = get_the_content();
+
+render('views/templates/custom.php', compact('title', 'content'));
+?>
+
+
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Wonder Webiste</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-0evHe/X+R7YkIZDRvuzKMRqM+OrBnVFBL6DOitfPri4tjfHxaWutUpFmBp4vmVor" crossorigin="anonymous">
+</head>
+
+<body>`
+  <h1>Intro</h1>
+  <p>Welkom op de site van wonder. Op deze site kan je eigenlijk reviews zien van mensen uit de organisatie wonder</p>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-pprn3073KE6tl6bjs2QrFaJGz5/SUsLqktiwsUTF55Jfv3qYSDhgCecCxMW52nD2" crossorigin="anonymous"></script>
+</body>
+
+</html>
